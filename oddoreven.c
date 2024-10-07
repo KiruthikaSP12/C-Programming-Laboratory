ac3ef547e5d224a22e09bd0b39edc87c29048c06
KiruthikaSP12 <kiruthikabhuvanesh8507@gmail.com>
2024-10-07 22:35:20 +0530

    Create oddoreven.c

---
+++ oddoreven.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    scanf("%d",&a);
+    if(a%2!=0)
+    {
+        printf("The number is odd");
+    }
+    else
+    {
+        printf("The number is even");
+    }
+    return 0;
+}
